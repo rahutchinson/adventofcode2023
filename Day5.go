5145271cd4c76c9fbd5836d1d489376af20fcef5
Ryan Hutchinson <ryanahutchinson@gmail.com>
2023-12-05 21:44:51 +0000

    Day 5 part 2 done

--- Day5.go
+++ Day5.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 	"unicode"
 )
 
@@ -15,67 +16,67 @@ func check(e error) {
 }
 
 type Map struct {
-	source      int64
-	destination int64
-	length      int64
+	source      int
+	destination int
+	length      int
 }
 
-func (m *Map) GetEndRangeSource() int64 {
+func (m *Map) GetEndRangeSource() int {
 	return m.source + m.length
 }
 
-func (m *Map) GetEndRangeDestination() int64 {
+func (m *Map) GetEndRangeDestination() int {
 	return m.destination + m.length
 }
 
 type SeedMap struct {
-	seed  int64
-	soil  int64
-	fert  int64
-	water int64
-	light int64
-	temp  int64
-	humid int64
-	loc   int64
+	seed  int
+	soil  int
+	fert  int
+	water int
+	light int
+	temp  int
+	humid int
+	loc   int
 }
 
-func trimAtoi(toTrim string) int64 {
+func trimAtoi(toTrim string) int {
 	num, _ := strconv.Atoi(strings.TrimSpace(toTrim))
-	return int64(num)
+	return int(num)
 
 }
 
 // inRange checks if a value is within a range of integers.
-func inRange(value, start, end int64) bool {
+func inRange(value, start, end int) bool {
 	return value >= start && value <= end
 }
 
-func AbsInt64(n int64) int64 {
+func Absint(n int) int {
 	if n < 0 {
 		return -n
 	}
 	return n
 }
 
-func mapToDestination(m []Map, sourceVal int64) int64 {
+func mapToDestination(m []Map, sourceVal int) int {
 	for _, ma := range m {
 		if inRange(sourceVal, ma.source, ma.GetEndRangeSource()) {
-			var diff int64
-			var dest int64
+			var diff int
+			var dest int
 			diff = ma.GetEndRangeSource() - sourceVal
 			dest = ma.GetEndRangeDestination() - diff
 			return dest
-
 		}
 	}
 	return sourceVal
 }
 
 func main() {
+	startTime := time.Now()
 	dat, err := os.ReadFile("./inputs/day5/input.txt")
 	check(err)
 	temp := strings.Split(string(dat), "\n")
-	var seeds []int64
+	var seeds []int
 	mapOfLists := make(map[string][]Map)
 	currentMap := ""
 	for _, line := range temp {
@@ -84,7 +85,7 @@ func main() {
 			nums := strings.Split(strings.TrimSpace(split[1]), " ")
 			for _, num := range nums {
 				conv, _ := strconv.Atoi(num)
-				seeds = append(seeds, int64(conv))
+				seeds = append(seeds, int(conv))
 			}
 			continue
 		}
@@ -98,26 +99,29 @@ func main() {
 		if unicode.IsNumber(rune(line[0])) {
 			nums := strings.Split(strings.TrimSpace(line), " ")
 			mapOfLists[currentMap] = append(mapOfLists[currentMap], Map{trimAtoi(nums[1]), trimAtoi(nums[0]), trimAtoi(nums[2])})
+			continue
 		}
 	}
 
 	minLocation := SeedMap{}
 	minLocation.loc = 203685477580700
-	for _, seed := range seeds {
-		ss := SeedMap{}
-		ss.seed = seed
-		ss.soil = mapToDestination(mapOfLists["seed-to-soil map"], seed)
-		ss.fert = mapToDestination(mapOfLists["soil-to-fertilizer map"], ss.soil)
-		ss.water = mapToDestination(mapOfLists["fertilizer-to-water map"], ss.fert)
-		ss.light = mapToDestination(mapOfLists["water-to-light map"], ss.water)
-		ss.temp = mapToDestination(mapOfLists["light-to-temperature map"], ss.light)
-		ss.humid = mapToDestination(mapOfLists["temperature-to-humidity map"], ss.temp)
-		ss.loc = mapToDestination(mapOfLists["humidity-to-location map"], ss.humid)
-		fmt.Println(ss)
-		if ss.loc < minLocation.loc {
-			minLocation = ss
+	for i := 0; i < len(seeds)-1; i += 2 {
+		for j := 0; j < int(seeds[i+1]); j++ {
+			ss := SeedMap{}
+			ss.seed = seeds[i] + int(j)
+			ss.soil = mapToDestination(mapOfLists["seed-to-soil map"], ss.seed)
+			ss.fert = mapToDestination(mapOfLists["soil-to-fertilizer map"], ss.soil)
+			ss.water = mapToDestination(mapOfLists["fertilizer-to-water map"], ss.fert)
+			ss.light = mapToDestination(mapOfLists["water-to-light map"], ss.water)
+			ss.temp = mapToDestination(mapOfLists["light-to-temperature map"], ss.light)
+			ss.humid = mapToDestination(mapOfLists["temperature-to-humidity map"], ss.temp)
+			ss.loc = mapToDestination(mapOfLists["humidity-to-location map"], ss.humid)
+			if ss.loc < minLocation.loc {
+				minLocation = ss
+			}
 		}
+		fmt.Println(minLocation)
 	}
-	fmt.Println(minLocation.loc)
-
+	duration := time.Since(startTime)
+	fmt.Println("Execution time:", duration)
 }
